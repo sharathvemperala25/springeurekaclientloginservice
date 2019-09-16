fd56dfdee3d0ed5c84f32491f5adb117e9378e91
User1 <anuradha.krishnanov7@gmail.com>
2019-09-16 15:19:58 +0530

    initial setup for login service

---
+++ src/test/java/com/springeurekaclientLoginservice/springeurekaclientloginservice/SpringeurekaclientloginserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springeurekaclientLoginservice.springeurekaclientloginservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringeurekaclientloginserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
